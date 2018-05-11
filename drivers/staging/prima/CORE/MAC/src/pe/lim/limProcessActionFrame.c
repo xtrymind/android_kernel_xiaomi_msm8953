2b9559bb0856093f1d4300b33738e254bacd722b
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-05-11 20:01:11 +0700

    prima: Remove unnecessary parentheses

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessActionFrame.c
@@ -878,9 +878,9 @@ __limProcessDelTsReq(tpAniSirGlobal pMac, tANI_U8 *pRxPacketInfo,tpPESession pse
     tsinfo = delts.wmeTspecPresent ? &delts.tspec.tsinfo : &delts.tsinfo;
 
     // if no Admit Control, ignore the request
-    if ((tsinfo->traffic.accessPolicy == SIR_MAC_ACCESSPOLICY_EDCA))
+    if (tsinfo->traffic.accessPolicy == SIR_MAC_ACCESSPOLICY_EDCA)
     {
-    
+
         if (upToAc(tsinfo->traffic.userPrio) >= MAX_NUM_AC)
         {
             limLog(pMac, LOGW, FL("DelTs with UP %d has no AC - ignoring request"),
